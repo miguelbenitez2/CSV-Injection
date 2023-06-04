ad1681e5dfea6cf45163b05fb7d2e68044f2dcd0
Miguel Benitez <miguelbenitez23.mb@gmail.com>
2023-06-04 17:45:58 +0200

    First Steps

--- Hacking Etico/Program.cs
+++ Hacking Etico/Program.cs
@@ -1,4 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Hacking_Etico;
 
-// Prueba
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<ValueClass> list = new List<ValueClass>();
+
+        ValueClass value1 = new ValueClass("@+Miguel", "1009");
+        ValueClass value2 = new ValueClass("=-Alma", "==3445");
+        ValueClass value3 = new ValueClass("+-Pepe", "@@23456");
+        ValueClass value4 = new ValueClass("=@Manolo", "++23556");
+        ValueClass value5 = new ValueClass("\nJijuile", "\t--234");
+
+        list.Add(value1);
+        list.Add(value2);
+        list.Add(value3);
+        list.Add(value4);
+        list.Add(value5);
+
+        foreach (var info in list)
+        {
+            //Value Name
+            while (info.name.StartsWith("+") || info.name.StartsWith("-") || info.name.StartsWith("=")
+                || info.name.StartsWith("@") || info.name.StartsWith("+") || info.name.StartsWith("\n")
+                || info.name.StartsWith("\t") || info.name.StartsWith("\n") || info.name.StartsWith(";"))
+            {
+                info.name = info.name.Substring(1);
+            }
+
+            //Value ID
+            while (info.id.StartsWith("+") || info.id.StartsWith("-") || info.id.StartsWith("=")
+                || info.id.StartsWith("@") || info.id.StartsWith("+") || info.id.StartsWith("\n")
+                || info.id.StartsWith("\t") || info.id.StartsWith("\n") || info.id.StartsWith(";"))
+            {
+                info.id = info.id.Substring(1);
+            }
+        }
+
+        foreach (var info in list)
+        {
+            Console.WriteLine($"Name {info.name} --- ID {info.id}");
+        }
+        
+    }
+}
---
+++ Hacking Etico/ValueClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacking_Etico
+{
+    public class ValueClass
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+
+        public ValueClass(string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+
+    }
+}
